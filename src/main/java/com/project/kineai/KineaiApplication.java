4366ce091bea37dd7537b84c525ce96681036451
HamzaElabboubi <elabboubihamza8@gmail.com>
2026-05-12 12:41:59 +0100

    first commit

---
+++ src/main/java/com/project/kineai/KineaiApplication.java
@@ -0,0 +1,13 @@
+package com.project.kineai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KineaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KineaiApplication.class, args);
+	}
+
+}
